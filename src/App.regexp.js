adaac5d66ae5ed968a9c2e195bde1eff34181390
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-03-15 13:47:19 +0800

    feat: Parse query parameters

---
+++ src/App.regexp.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import './App.css';
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/:a(\d{2})/:b([a-z]+)" render={({match}) => (
+        <h1>
+          paramA: {match.params.a}<br />
+          paramB: {match.params.b}
+        </h1>
+      )} />
+    </div>
+  </Router>
+);
+
+export default App;
